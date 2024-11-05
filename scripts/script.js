9543eb85319b17d985fbb342f703496777bd0d07
Anthony CARRAUD <a.carraud@gmail.com>
2024-11-05 16:27:04 +0000

    Ajout des exercices 1 à 3 sur les boucles

--- scripts/script.js
+++ scripts/script.js
@@ -105,3 +105,73 @@
 //     default:
 //         alert("Opérateur inconnu");
 // }
+
+// Exercice 4 :
+
+// //Je déclare 6 variables 
+// let prix_Unitaire = parseFloat(prompt("Saisisser le prix unitaire du produit"));
+// let quantite = parseInt(prompt("Saisisser la quantité déirée"));
+// let tot = prix_Unitaire * quantite;
+// let fd_Port;
+// let pap;
+// let remise;
+
+// //Si tot est supérieur à 500 alors frais de port est égale à 0
+// if (tot > 500) {
+//     fd_Port = 0;
+// }
+// //Sinon frais de port = tot * 0.02 pour que les frais de port corespondent à 2% du tot toutefois si ça donne des frais de port inférieur à 6 alors frais de port = 6
+// else {
+//     fd_Port = tot * 0.02;
+//     if (fd_Port > 6) {
+//         fd_Port = 6
+//     }
+// }
+// //Switch pour calculer la remise
+// switch (remise){
+//     case tot < 100 :
+//         remise = 0;
+//         break;
+//     case tot >=100 && tot <=200 :
+//         remise = tot * 0.05;
+//         break;
+//     default :
+//     remise = tot * 0,10;          
+
+// }
+// //Le prix à payer = tot - remise + frais de port
+// pap =  parseFloat(tot - remise + fd_Port);
+
+// //Affiche le prix à payer après avoir effectué les calculs. 
+// alert("Le prix à payer est de "+ pap + "€");
+
+// // Exercice 1 Boucles :
+
+// //Je déclare 3 variables
+// let sprenom;
+// let iNb=1; 
+// let liste = "";
+
+// // Tant que sprénom est différent de "" effectue cette boucle 
+// while (sprenom!="")
+// {
+// sprenom = (prompt("Saisir un prénom, saisie numéro "+iNb));
+// liste +=  "/" + sprenom;
+// iNb ++;
+// }
+// console.log((iNb-2) + liste);
+
+// Exercice 2-3 Boucles :
+
+let iN = parseInt(prompt("Saisir un nombre entier"))
+let iNb = 0;
+// A partir de l'exercice 3
+let iSnbe = 0;
+let i_Anb = 0;
+
+while (iNb < iN) {
+    console.log(iNb);
+    iNb++;
+     }
+iSnbe = ((iNb-1) * iN) / 2;
+console.log("la somme des entiers inférieur au nombre que vous avez saisi est de : " + iSnbe);
